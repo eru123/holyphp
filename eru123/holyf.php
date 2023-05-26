a3e7fe086b70a106e235c6005b7aa8656b6f4635
ipp-jericho <108918542+ipp-jericho@users.noreply.github.com>
2023-05-26 17:53:14 +0800

    added env get/set

--- eru123/holyf.php
+++ eru123/holyf.php
@@ -1,7 +1,24 @@
 <?php
 
 use eru123\config\BaseConfig;
+use eru123\helper\ArrayUtil;
 
-function config(...$args) {
-    return BaseConfig::get(...$args);
+function config(string $key, $default = null)
+{
+    return BaseConfig::get($key, $default);
+}
+
+function config_set(string $key, $value)
+{
+    return BaseConfig::set($key, $value);
+}
+
+function env(string $key = null, $default = null)
+{
+    return ArrayUtil::get($_ENV, $key, $default);
+}
+
+function env_set(string $key, $value)
+{
+    return ArrayUtil::set($_ENV, $key, $value);
 }
