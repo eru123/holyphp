173042f0373922ef3d85d88f0db34a449b7f80bc
eru123 <yeoligoakino@gmail.com>
2023-07-25 18:00:13 +0800

    update

--- eru123/router/Router.php
+++ eru123/router/Router.php
@@ -476,7 +476,6 @@ class Router
     {
         http_response_code($code);
         $title = htmlspecialchars($title);
-        $message = htmlspecialchars($message);
         echo "<!DOCTYPE html> <html lang=\"en\"> <head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>$title</title> <style> body { font-family: sans-serif; background-color: #f1f1f1; } h1 { text-align: center; margin-top: 100px; } p { text-align: center; font-size: 18px; } </style></head><body><h1>$title</h1><p>$message</p></body></html>";
         exit;
     }
